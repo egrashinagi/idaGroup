bf2ba0d4a508efde96b22828c04a363c88cf8297
Egrashina Galina <egrashina.gi@gmail.com>
2018-01-20 20:17:32 +0300

    мелкие правки

--- src/components/card/card.js
+++ src/components/card/card.js
@@ -1,1 +1,9 @@
-
+function validateForm()
+{
+    const x=document.forms["data"]["number-card","month", "year", "name", "code" ].value;
+    if (x===null || x==="")
+    {
+        alert("Необходимо заполнить поле!");
+        return false;
+    }
+}
